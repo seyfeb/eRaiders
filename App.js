f32b7f95faf58427afa29442698da6231dd48f2d
Sebastian Fey <info@sebastianfey.de>
2018-10-20 16:54:42 +0200

    Added stuff

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
 //  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {ActivityIndicator, FlatList, Platform, StyleSheet, Text, View, Button} from 'react-native';
 import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs'
 import rentalStackNav from './rentalStackNavigation';
 
@@ -25,13 +25,70 @@ class BoardRentalScreen extends React.Component {
 }
 
 class BoardUnlockScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
+  constructor(props) {
+    super(props);
+    this.state = { dataSource: '', isLoading: true };
+  }
+
+  componentDidMount(){
+    return fetch('http://10.0.25.125:3000/api/boards/geo/49.4543640/11.0512897')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.boards,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  unlockBoard()
+  {
+
+  }
+
+  highlightBoard()
+  {
+    // Change LED color
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <View style={{flexWrap: 'wrap', alignItems: 'flex-start',flexDirection:'row'}}>
+            <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>{item.id} {item.state} {item.costPerHour}</Text>
+            <View style={{width:100, margin: 5}}><Button onPress={() => this.highlightBoard() } title="Highlight"/></View>
+            <View style={{width:80, margin: 5}}><Button onPress={() => this.unlockBoard() } title="Unlock"/></View>
+            </View>}
+          keyExtractor={({id}, index) => id}
+        />
       </View>
     );
   }
+
+
+
+
+
 }
 
 class BoardInfoScreen extends React.Component {
@@ -43,31 +100,6 @@ class BoardInfoScreen extends React.Component {
     );
   }
 }
-
-
-
-
-// const tabNav = TabNavigator({
-//     TabItem1: {
-//         screen: stackNav,
-//         navigationOptions: {
-//             tabBarLabel:"Tab 1",
-//             tabBarIcon: ({ tintColor }) => <Icon name={"glass"} size={30} color={tintColor} />
-//         }
-//     }
-
-//     ///... add more tabs here
-
-// }, {
-//         tabBarOptions: {
-//             activeTintColor: '#222',
-//         }
-// });
-
-
-
-
-
 
 
 
@@ -85,3 +117,18 @@ export default TabNavigator({
 //         }
 // }
 );
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  available: {
+    color: 'green'
+  },
+  busy: {
+    color: 'red'
+  }
+});
--- rentalStackNavigation.js
+++ rentalStackNavigation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, DatePickerAndroid, TimePickerAndroid, StyleSheet, TouchableOpacity, Text, TextInput, View } from 'react-native';
 import { StackNavigator} from 'react-navigation'
 
+
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 Mapbox.setAccessToken('pk.eyJ1Ijoic2V5ZmViIiwiYSI6ImNqbmg2bmd0dDA3YmUzcHI3ZDA4OW9vb3gifQ.nvQYS5L65MsINHCwMFmDsQ');
 
@@ -9,7 +10,7 @@ Mapbox.setAccessToken('pk.eyJ1Ijoic2V5ZmViIiwiYSI6ImNqbmg2bmd0dDA3YmUzcHI3ZDA4OW
 class PickupDateTimeSelectionScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { text: '', year:'2018', month:'10', day:'21', hour:'18', minute:'00' };
+        this.state = { text: '', year:'2018', month:'10', day:'21', hour:'18', minute:'00', durationHour:0, durationMinute:0 };
     }
 
     async openAndroidDatePicker() {
@@ -42,58 +43,50 @@ class PickupDateTimeSelectionScreen extends React.Component {
       }
     }
 
+    async openAndroidTimePickerForDuration() {
+        try {
+          const {action, hour, minute} = await TimePickerAndroid.open({
+            hour: 0,
+            minute: 0,
+            is24Hour: true, // Will display '2 PM'
+          });        
+            this.setState(previousState => {
+                return { durationHour: hour, durationMinute: minute };
+            });
+          if (action !== TimePickerAndroid.dismissedAction) {
+          }
+        } catch ({code, message}) {
+          console.warn('Cannot open time picker', message);
+      }
+    }
+
 
 
   render() {
     return (
       <View style={styles.container}>
-          <Text style={{height: 40, borderColor: 'gray', borderWidth: 0}}>Select your arrival date & time!</Text>
-            <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white', minWidth: 200}}
-                onChangeText={(text) => this.setState({text})}
-                value={this.state.text}
-            />
         {/*<Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
             zoomLevel={15}
             centerCoordinate={[11.256, 43.770]}
             style={styles.container}>
         </Mapbox.MapView>*/}
-        <Button onPress={() => this.openAndroidDatePicker() } title="Select date"/>
-        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0}}>{this.state.year}-{this.state.month}-{this.state.day}</Text>
+        <View style={styles.buttons}>
+        <Button onPress={() => this.openAndroidDatePicker() } title="Select date"/></View>
+        <Text style={styles.label}>{this.state.year}-{this.state.month}-{this.state.day}</Text>
 
-        <Button onPress={() => this.openAndroidTimePicker() } title="Select time"/>
-        <Text style={[styles.labels, { height: 40, borderColor: 'gray', borderWidth: 0}]}>{this.state.hour}:{this.state.minute}</Text>
+        <View style={styles.buttons}>
+        <Button onPress={() => this.openAndroidTimePicker() } title="Select time"/></View>
+        <Text style={styles.label}>{this.state.hour}:{this.state.minute}</Text>
 
-        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Expected rental duration</Text>
-         <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white', minWidth: 200}}
-                onChangeText={(duration) => this.setState({duration})}
-                value={this.state.duration}
-            />
+        <View style={styles.buttons}>
+        <Button onPress={() => this.openAndroidTimePickerForDuration() } title="Expected rental duration"/></View>
+        <Text style={styles.label}>{this.state.durationHour}:{this.state.durationMinute}</Text>
 
-        <Button onPress={(day) => this.props.navigation.navigate('OverviewScreen', {day: day})} title="Next"/>
+        <View style={styles.buttons}>
+        <Button onPress={() => this.props.navigation.navigate('Overview', this.state)} title="Next"/></View>
       </View>
     );
-
-
-    {/*
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0}}>Select your pick-up location!</Text>
-        <TextInput
-            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white', minWidth: 200}}
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
-        />
-        <Mapbox.MapView
-            styleURL={Mapbox.StyleURL.Street}
-            zoomLevel={15}
-            centerCoordinate={[11.256, 43.770]}
-            style={styles.container}>
-        </Mapbox.MapView>
-      </View>
-    );*/}
   }
 }
 
@@ -107,10 +100,11 @@ class PickupLocationSelectionScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <Text style={{height: 40, borderColor: 'gray', borderWidth: 0}}>Select your pick-up location!</Text>
+
             <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white', minWidth: 200}}
+                style={{height:80, padding:15,marginLeft:10, marginRight:10, marginBottom:40, marginTop:60, height: 50, border: 'none', borderColor: 'none', borderWidth: 1, backgroundColor: 'white', minWidth: 200, fontSize:18}}
                 onChangeText={(text) => this.setState({text})}
+                placeholder='Select your pickup location!'
                 value={this.state.text}
             />
         {/*<Mapbox.MapView
@@ -145,17 +139,116 @@ class PickupLocationSelectionScreen extends React.Component {
 class PickupOverviewScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { da: '' };
+        // this.state = { day: this.props.navigation.state.params.day };
+        this.state = this.props.navigation.state.params;
     }
 
 
   render() {
     return (
       <View style={styles.container}>
-          <Text style={{height: 40, borderColor: 'gray', borderWidth: 0}}>{this.props.day}
-{this.props.navigation.state.day}
-{this.props.navigation.state.params.day}</Text>
-        <Button onPress={() => this.props.navigation.navigate('DateTime')} title="Book board!"/>
+        <Text style={{fontSize:20, height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Rental date</Text>
+        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>{this.state.year}-{this.state.month}-{this.state.day}</Text>
+        <Text style={{fontSize:20, height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Rental time</Text>
+        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>{this.state.hour}:{this.state.minute}</Text>
+        <Text style={{fontSize:20, height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Expected rental duration</Text>
+        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>{this.state.durationHour}:{this.state.durationMinute}</Text>
+        <Text style={{fontSize:20, height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Price per minute</Text>
+        <Text style={{height: 80, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>0.10€/min.</Text>
+        <Button onPress={() => this.props.navigation.navigate('Success', this.state)} title="Book board!"/>
+      </View>
+    );
+  }
+}
+class SuccessScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.state = { day: this.props.navigation.state.params.day };
+        this.state = this.props.navigation.state.params;
+        // console.warn(this.state)
+        this.state.remainingTime = '0';
+
+
+    
+    setInterval(() => {
+        let remString = '';
+        let now = new Date();
+        // this.state.year+'-'+this.state.month+'-'+this.state.day+'T'+this.state.hour+':'+this.state.minute+':00'
+        let reservationDate = new Date();
+
+        console.warn(this.state);
+        reservationDate.setUTCFullYear(this.state.year);
+        reservationDate.setUTCMonth(this.state.month-1);
+        reservationDate.setUTCDate(this.state.day);
+        reservationDate.setUTCHours(this.state.hour);
+        reservationDate.setUTCMinutes(this.state.minute);
+        // console.warn(now);
+        console.warn(reservationDate);
+
+        let dateDifference = reservationDate - now;
+
+        if ( dateDifference <= 0 )
+        {
+            remString = 'Board arrived. Have fun!';
+        }
+        else
+        {
+            // console.warn(now.toISOString());
+            // console.warn(reservationDate.toISOString());
+
+            let remDays = Math.floor(dateDifference / (1000*60*60*24));
+            // // let remMinutes = Math.floor(dateDifference / (1000*60))- remDays*(1000*60*60*24)- remHours*(1000*60*60);
+            // // let remSeconds = Math.floor(dateDifference / 1000)- remDays*(1000*60*60*24)- remHours*(1000*60*60)- remMinutes*(1000*60);
+            // let remDays = Math.floor(dateDifference / (1000*60*60*24));
+            let remHours = Math.floor(dateDifference / (1000*60*60)) - remDays*(1000*60*60);
+            // // let remHours = Math.floor(dateDifference / (1000*60*60));
+            // let remMinutes = Math.floor(dateDifference / (1000*60));
+            // let remSeconds = Math.floor(dateDifference / 1000);
+
+
+            if (remDays > 0) {
+                remString = remDays + 'd ' + remHours + 'h';
+                console.warn('a');
+            }
+            else 
+            {
+                if (remHours > 0) {
+                    remString = remHours + 'h ' + remMinutes + 'min';
+                console.warn('b');
+                }
+                else
+                {
+                    if (remMinutes > 0) {
+                        remString = remMinutes + 'min ';
+                console.warn('c');
+                    }
+                    else
+                    {
+                        remString = 'Board arrived. Have fun!';
+                console.warn('d');
+                    }
+                }
+            }
+
+            // console.warn('days'+remDays+'h'+remHours+'min'+remMinutes);
+        }
+
+          this.setState(() => {
+            return { remainingTime: remString };
+          });
+        }, 2000);
+    }
+
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Your board has successfully been reserved and will be waiting at the requested location.</Text>
+        <Text style={{height: 40, borderColor: 'gray', borderWidth: 0, fontWeight: 'bold'}}>Your board arrives in</Text>
+        <Text style={{textAlign:'center', height: 60, borderColor: 'gray', borderWidth: 0, fontSize: 40, fontWeight: 'bold'}}>{this.state.remainingTime}</Text>
+        
       </View>
     );
   }
@@ -188,6 +281,12 @@ const rentalStackNav = StackNavigator({
           title: "Checkout",
         }),
     },
+    Success: {
+        screen: SuccessScreen,
+        navigationOptions: ({ navigation }) => ({
+          title: "Success",
+        }),
+    },
 
 });
 
@@ -196,7 +295,22 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   label: {
-    textAlign: 'center'
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 0,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  buttons: {
+    marginLeft: 15,
+    marginRight: 15,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+
   }
 });
 
